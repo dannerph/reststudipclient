05b8474341ead2c9dec64c399f214e5c2864946b
Philipp Danner <philipp@danner-web.de>
2015-04-14 18:52:01 +0200

    added some routes and ocumentation

--- routes/NewsMap.php
+++ routes/NewsMap.php
@@ -13,77 +13,49 @@
  * @category    Stud.IP
  * @package     ExtendedNews
  */
-class NewsMap extends RESTAPI\RouteMap {
+class SSFCoreMap extends RESTAPI\RouteMap {
+
+    //************************************************************************//
+    // Routes for file download core plugin
+    //************************************************************************//
 
     /**
-     * Validates a user until a given date
+     * Cheks for changes of Document after timestamp ind the current users
+     * courses. Return either an empty json or a tree over semester (root node),
+     * courses, folders, subfolders [...], Metadata of documents changed after
+     * timestamp
      *
-     * @put /card/validate/:username/:semester/:until/:status
+     * @get /ssf-core/documenttree/
+     * @get /ssf-core/documenttree/:timestamp
      */
-    public function validateCard($username, $semester, $until, $status) {
-        
-        // Lookup user
-        $user = User::findByUsername($username);
-        
-        // If card doesnt exist
-        if (!$user) {
-            $this->error(404, "User not found");
+    public function getDocumentTree($timestamp = null) {
+        if($timestamp === null){
+            // return whole tree with root of the current semester
         }
-
-        $validation = new CardportalValidation(array($user->id, $semester));
-        $validation->valid_until = $until;
-        $validation->status = $status;
-        $validation->store();
-        
-        // Remove all future validations
-        CardportalValidation::deleteBySQL("user_id = ? AND valid_until > ?", array($user->id, $until));
     }
-
+    
+    //************************************************************************//
+    // Routes for news core plugin
+    //************************************************************************//
+    
     /**
-     * Validates a card until a given date
+     * Returns unread news of the current user
+     * return value: array(id, topic, body, author (name), chdate, mkdate, 
+     * expire, ...)
      *
-     * @get /card/validate/:cardserialnumber
+     * @get /ssf-core/unreadnews
      */
-    public function getValidation($cardserialnumber) {
-        $card = CardportalCard::findBySQL('serialnumber = ?', array($cardserialnumber));
-
-        // If card doesnt exist
-        if (!$card) {
-            $this->error(404, "Id not found");
-        }
-
-        $card = current($card);
-        
-        // If card is not validateable
-        if (!$card->cardtype->validation) {
-            $this->error(404, "Card not validateable");
-        } 
-
-        return array(
-            'valid' => $card->getValidationDate() != "",
-            'text' => $card->getValidationString(),
-            'logo' => $card->getValidationDate() != "" && $card->cardtype->vbp_logo
-        );
-    }
-
-    /**
-     * Returns all printed cards
-     *
-     * @get /card/changed
-     * @get /card/changed/:timestamp
-     */
-    public function getChanged($timestamp = null) {
-        foreach (CardportalHelper::getChangedPrintedCards($timestamp) as $card) {
-            $output[] = array(
-                'id' => $card->id,
-                'serialnumber' => $card->serialnumber,
-                'cardnumber' => $card->cardnumber,
-                'status' => $card->status,
-                'user' => $card->user->username,
-                'ub' => $card->library_id
-            );
-        }
+    public function getUnreadNews() {
         return $output;
+    }
+    
+    /**
+     * Sets the news of the current user with the given new id $id to readed
+     *
+     * @put /ssf-core/setnewsreaded/:id
+     */
+    public function setNewsReaded($id = null) {
+        
     }
 
 }
